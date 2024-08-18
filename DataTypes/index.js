294538380e3d5f87a1b16c498c60d399e1d38c4b
RusheekeshGadge <gadgerusheekesh@gmail.com>
2024-08-18 12:52:07 +0530

    changes are done

---
+++ DataTypes/index.js
@@ -0,0 +1,13 @@
+"use strict";// treat code as newer version of javascript
+// alert(2 +2) we are using nodejs not browser
+
+let name = "Rusheekesh"
+let age = 20 
+let isLogged = false
+// number = 2 to power 53
+// Symbol; == uniquename
+let r;
+console.log(typeof r);
+console.log(typeof null);
+//js treats the null as object 
+// some experts say its the error
