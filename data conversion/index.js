bd7d70f0d52e6257d3c6cd44ca1f4e010b3a0c32
RusheekeshGadge <gadgerusheekesh@gmail.com>
2024-08-18 13:48:02 +0530

    conversion of data types

---
+++ data conversion/index.js
@@ -0,0 +1,15 @@
+let score = null;
+console.log(typeof score);
+let newnumber = Number(score)
+console.log(newnumber)
+// if 33a then the value is nan
+//if the value is in boolean true then it will be 1 
+// and if the value is in boolean false then it will be 0
+// if the value is in undefinesd then it will be undefined
+// if the type is null then it will be 
+
+let a =""
+//"" empty string gives value false
+// "e" if there is any thing in the string then it will be true
+let b = Boolean(a)
+console.log(b)
