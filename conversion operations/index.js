7528accec7c5ca53e8479b53fece41377601268c
RusheekeshGadge <gadgerusheekesh@gmail.com>
2024-08-18 14:03:23 +0530

    another conversions

---
+++ conversion operations/index.js
@@ -0,0 +1,14 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+//turns value into negative value
+
+let stringValue = "123"
+let numberValue = parseInt(stringValue)
+console.log(numberValue);
+// way to convert
+
+console.log("1" + 2) //12
+console.log("1" + 2 + 2)  //122 concatenatedthe value
+console.log(1+ 2 + "2")  //32 added first two values then concatenatedthe value
+
